24a0851fb0ac1f6487c9aec78d236c00c6ba652e
Neel Goyani <neelgoyani709@gmail.com>
2022-07-30 11:12:03 +0530

    Latest modified

--- Pr_2.java
+++ Pr_2.java
@@ -1,8 +1,20 @@
-import java.util.*;  
+/* 
+    Given a string, return a string made of the first 2 chars (if present), however include
+    first char only if it is 'o' and include the second only if it is 'z', so "ozymandias"
+    yields "oz".
+    startOz("ozymandias") → "oz"
+    startOz("bzoo") → "z"
+    startOz("oxx") → "o" 
+*/
+
+// Neel Goyani - 21ce036
+
+import java.util.*;
+
 public class Pr_2 {
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the String : ");
         String s = sc.nextLine();
@@ -12,17 +24,15 @@ public class Pr_2 {
         sc.close();
     }
 
-    public static String startOz(String a)
-        {   
-            String ans = "";
-            if(a.charAt(0)=='o')
-            ans=ans+'o';
+    public static String startOz(String a) {
+        String ans = "";
+        if (a.charAt(0) == 'o')
+            ans = ans + 'o';
 
-            if(a.charAt(1)=='z')
-            ans=ans+'z';
+        if (a.charAt(1) == 'z')
+            ans = ans + 'z';
 
         return ans;
-        }
-    
-}
+    }
 
+}
--- Pr_9.java
+++ Pr_9.java
@@ -1,19 +1,34 @@
-import java.util.*;
-public class Pr_9 {
-    public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        final int r = 9;
-        final int c = 9;
-        int arr[][]= new int[r][c];
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-        for (int i = 0; i < r; i++) 
-        {
-            for (int j = 0; j < c; j++) 
-            {
-                arr[i][j] = obj.nextInt(); 
+public class Pr_9 
+{
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("sudokuNuInput.sudoku");
+        Scanner sc = new Scanner(file);
+        final int a = 9;
+
+        int[] ekThiNav = new int[9];
+        for (int i = 0; i < ekThiNav.length; i++)
+            ekThiNav[i] = i + 1;
+
+        int[][] sudoku = new int[a][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                sudoku[i][j] = sc.nextInt();
+                if (sudoku[i][j] >= 10) {
+                    System.err.println("Element can not be greather then 9");
+                    System.exit(1);
+                }
             }
         }
 
-        obj.close();
+        // checking row
+        // for (int i = 0; i < a; i++) {
+        //     for (int j = 0; j < a; j++) {
+                
+        //     }
+        // }
     }
 }
