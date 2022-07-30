79ef05e9ff880d0878afe722c4e7a3b1127ccf4e
Neel Goyani <neelgoyani709@gmail.com>
2022-07-30 11:34:42 +0530

    Pr_6 latest commit

--- Pr_6.java
+++ Pr_6.java
@@ -1,8 +1,18 @@
+/*
+    Given an array of strings, return a new array without the strings that are equal to
+    the target string. One approach is to count the occurrences of the target string, make
+    a new array of the correct length, and then copy over the correct strings.
+    wordsWithout(["a", "b", "c", "a"], "a") → ["b", "c"]
+    wordsWithout(["a", "b", "c", "a"], "b") → ["a", "c", "a"]
+    wordsWithout(["a", "b", "c", "a"], "c") → ["a", "b", "a"]
+*/
+
+// Neel Goyani - 21ce036
+
 import java.util.*;
 
 public class Pr_6 {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of array : ");
         int a = sc.nextInt();
@@ -17,30 +27,26 @@ public class Pr_6 {
         sc.close();
     }
 
-    static void wordsWithout(String x[],String y)
-    {   int count=0;
-        for(int i=0;i<x.length;i++)
-        {
-            if(x[i].equals(y))
-            {
+    static void wordsWithout(String x[], String y) {
+        int count = 0;
+        for (int i = 0; i < x.length; i++) {
+            if (x[i].equals(y)) {
                 count++;
             }
         }
-        String[] d=new String[x.length-count];
-        int j=0;
-        for(int i=0;i<x.length;i++)
-        {
-            if(!x[i].equals(y))
+        String[] d = new String[x.length - count];
+        int j = 0;
+        for (int i = 0; i < x.length; i++) {
+            if (!x[i].equals(y))
             // {}
             // else
             {
-                d[j]=x[i];
+                d[j] = x[i];
                 j++;
             }
         }
         System.out.println("New Array is :");
-        for(String element:d)
-        {
+        for (String element : d) {
             System.out.println(element);
         }
 
