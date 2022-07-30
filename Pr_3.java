ba0f5535f3bd14c52499b35319844b17329535f7
Neel Goyani <neelgoyani709@gmail.com>
2022-07-30 11:16:58 +0530

    Latest Commit

--- Pr_3.java
+++ Pr_3.java
@@ -1,3 +1,14 @@
+/* 
+    Given two non-negative int values, return true if they have the same last digit, such
+    as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 %
+    10 is 7.
+    lastDigit(7, 17) → true
+    lastDigit(6, 17) → false
+    lastDigit(3, 113) → true
+*/
+
+// Neel Goyani - 21ce036
+
 import java.util.*;
 
 public class Pr_3 {
@@ -20,5 +31,4 @@ public class Pr_3 {
         else
             System.out.println("false");
     }
-
 }
