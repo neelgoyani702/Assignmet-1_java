a6e6f6f09845d031da9f6231c90c9004ddc04511
Neel Goyani <neelgoyani709@gmail.com>
2022-09-28 09:52:47 +0530

    13-09-2022

---
+++ Part 7/Pr_6.java
@@ -0,0 +1,29 @@
+import java.util.*;
+// import java.lang.*;
+
+public class Pr_6 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int g1 = obj.nextInt();
+            for (int j = 0; j < g1; j++) {
+                int arr[] = new int[g1];
+            }
+            // for (int j = 0; j < g1; j++) {
+            //     for (int j2 = 0; j2 < g1; j2++) {
+            //         int a = (int) Math.pow(2, g1);
+            //         int arr[] = new int[a];
+
+            //         arr[]
+
+            //     }
+
+            // }
+            for (int j = 0; j < args.length; j++) {
+                
+            }
+        }
+    }
+}
---
+++ Practice Assignment/Pr_1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Pr_1 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        
+        obj.close();
+    }   
+}
